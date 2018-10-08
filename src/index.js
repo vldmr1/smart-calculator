e0e715a4553a88d156ad02e49b7536090ddf8bcf
vldmr1 <vldmr.one@gmail.com>
2018-10-08 19:50:56 +0300

    smart-calculator solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,127 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stackNumber = [initialValue];
+    this.stackOp = [];
   }
 
   add(number) {
-    // your implementation
+    if (this.stackOp.length === 0) {
+      this.stackOp.push('+');
+      this.stackNumber.push(number);
+    } else if (['+', '-', '*', '/', '**'].includes(this.stackOp[this.stackOp.length-1])) {
+      let poppedNumber_1 = this.stackNumber.pop();
+      let poppedNumber_2 = this.stackNumber.pop();
+      let poppedOp = this.stackOp.pop();
+      this.stackNumber.push(this.calculate(poppedOp, poppedNumber_2, poppedNumber_1));
+      this.stackNumber.push(number);
+      this.stackOp.push('+');
+    } 
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    if (!this.stackOp.length) {
+      this.stackOp.push('-');
+      this.stackNumber.push(number);
+    } else if (['+', '-', '*', '/', '**'].includes(this.stackOp[this.stackOp.length-1])) {
+      let poppedNumber_1 = this.stackNumber.pop();
+      let poppedNumber_2 = this.stackNumber.pop();
+      let poppedOp = this.stackOp.pop();
+      this.stackNumber.push(this.calculate(poppedOp, poppedNumber_2, poppedNumber_1));
+      this.stackNumber.push(number);
+      this.stackOp.push('-');
+    } 
+    return this; 
   }
 
   multiply(number) {
-    // your implementation
+    if (!this.stackOp.length || ['+', '-'].includes(this.stackOp[this.stackOp.length-1])) {
+      this.stackOp.push('*');
+      this.stackNumber.push(number);
+    } else if (['*', '/', '**'].includes(this.stackOp[this.stackOp.length-1])) {
+      let poppedNumber_1 = this.stackNumber.pop();
+      let poppedNumber_2 = this.stackNumber.pop();
+      let poppedOp = this.stackOp.pop();
+      this.stackNumber.push(this.calculate(poppedOp, poppedNumber_2, poppedNumber_1));
+      this.stackNumber.push(number);
+      this.stackOp.push('*');
+    } 
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    if (!this.stackOp.length || ['+', '-'].includes(this.stackOp[this.stackOp.length-1])) {
+      this.stackOp.push('/');
+      this.stackNumber.push(number);
+    } else if (['*', '/', '**'].includes(this.stackOp[this.stackOp.length-1])) {
+      let poppedNumber_1 = this.stackNumber.pop();
+      let poppedNumber_2 = this.stackNumber.pop();
+      let poppedOp = this.stackOp.pop();
+      this.stackNumber.push(this.calculate(poppedOp, poppedNumber_2, poppedNumber_1));
+      this.stackNumber.push(number);
+      this.stackOp.push('/');
+    }
+    return this;  
   }
 
   pow(number) {
-    // your implementation
+    let poppedNumber_1 = number;
+    let poppedNumber_2 = this.stackNumber.pop();
+    this.stackNumber.push(this.calculate('**', poppedNumber_2, poppedNumber_1));
+    return this;
+  }
+    
+
+  calculate(operator, x, y) {
+    switch(operator) {
+      case '+':
+        return x + y
+        break;
+      case '-':
+        return x - y
+        break;
+      case '*':
+        return x * y
+        break;
+      case '/':
+        return x / y
+        break;
+      case '**':
+        return x ** y
+        break;
+    }
+  }
+
+  valueOf() {
+    let result = 0;
+    let poppedNumber_1;
+    let poppedNumber_2;
+    let poppedOp;
+    while (this.stackOp.length) {    
+      poppedNumber_1 = this.stackNumber.pop();
+      poppedNumber_2 = this.stackNumber.pop();
+      poppedOp = this.stackOp.pop()
+      result = this.calculate(poppedOp, poppedNumber_2, poppedNumber_1)
+      this.stackNumber.push(result);
+    }
+  
+  return result
   }
 }
 
 module.exports = SmartCalculator;
+
+/* const calculator = new SmartCalculator(10);
+
+    const value = calculator
+      .multiply(2)
+      .pow(2)
+      .subtract(95)
+      .subtract(56)
+      .pow(2)
+      .pow(2)
+      .pow(1)
+      .multiply(1);
+      
+
+console.log(value); */
